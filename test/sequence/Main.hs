34bea4509020d59256b731dc7b97679fb01b6396
RubenVerg <RubenVerg@outlook.com>
2026-01-21 02:42:45 +0100

    Try a different way...

--- test/sequence/Main.hs
+++ test/sequence/Main.hs
@@ -11,15 +11,14 @@ import Control.Monad
 import Control.Exception
 import System.Directory
 import Numeric.Natural
+import System.Environment
 import Text.Printf
 import Network.HTTP.Simple
-import Network.HTTP.Client.Conduit (Request(checkResponse), Response (responseStatus, responseBody), HttpExceptionContent (StatusCodeException))
-import Network.HTTP.Types.Status (Status(..))
 import Data.Aeson
 import Data.Aeson.Types (prependFailure, typeMismatch)
 import qualified Data.Aeson.KeyMap as KM
 import qualified Data.Vector as V
-import qualified Data.ByteString.Char8 as BS
+import Data.Char (isSpace)
 
 newtype R = R [Integer]
 
@@ -42,32 +41,56 @@ makePath :: Natural -> FilePath
 makePath = printf "%s/%d" rootPath
 
 makeRequest :: Natural -> IO Request
-makeRequest = fmap (\req -> req{ checkResponse = \rq rs -> do 
-  let Status code _ = responseStatus rs
-  guard $ code < 200 || code >= 300
-  body <- responseBody rs
-  BS.putStrLn body
-  let ex = StatusCodeException (void rs) body
-  throwIO $ HttpExceptionRequest rq ex }) . parseRequest . printf "https://oeis.org/search?fmt=json&q=id:A%06d"
+makeRequest = parseRequestThrow . printf "https://oeis.org/search?fmt=json&q=A%06d"
 
-getSequence' :: Natural -> IO (Maybe [Integer])
-getSequence' num = do
+getSequenceFromAPI :: FilePath -> Natural -> IO (Maybe [Integer])
+getSequenceFromAPI p num = do
+  oeis <- makeRequest num >>= fmap getResponseBody . httpJSON
+  case oeis of
+    Nothing -> pure Nothing
+    Just (R seqData) -> do
+      Just seqData <$ writeFile p (show seqData) -- `catch` (\(_ :: IOException) -> pure ())
+
+isActions :: IO Bool
+isActions = do
+  env <- lookupEnv "GITHUB_ACTIONS"
+  pure $ case env of
+    Just "true" -> True
+    _ -> False
+
+makeDataPath :: Natural -> FilePath
+makeDataPath num =
+  let str = printf "A%06d" num :: String
+  in printf "oeisdata/seq/%s/%s.seq" (take 4 str) str
+
+getSequenceFromData :: FilePath -> Natural -> IO (Maybe [Integer])
+getSequenceFromData p num = do
+  let dp = makeDataPath num
+  df <- try $ readFile dp :: IO (Either IOException String)
+  case df of
+    Left _ -> pure Nothing
+    Right str -> do
+      let s = read $ printf "[%s]" $ concatMap (filter (not . isSpace) . drop (length ("%S A000000 " :: String))) $ filter (\line -> take 2 line `elem` ["%S", "%T", "%U"]) $ lines str
+      putStr $ show num ++ ": "
+      print s
+      Just s <$ writeFile p (show s)
+
+getSequence :: Natural -> IO (Maybe [Integer])
+getSequence num = do
   let p = makePath num
   d <- try $ readFile p :: IO (Either IOException String)
   case d of
     Right str -> pure $ Just $ read str
     Left _ -> do
-      oeis <- makeRequest num >>= fmap getResponseBody . httpJSON
-      case oeis of
-        Nothing -> pure Nothing
-        Just (R seqData) -> do
-          Just seqData <$ writeFile p (show seqData) -- `catch` (\(_ :: IOException) -> pure ())
+      act <- isActions
+      if act then getSequenceFromData p num
+      else getSequenceFromAPI p num
 
 main :: IO ()
 main = hspec $ do
   runIO $ createDirectoryIfMissing True rootPath
   forM_ sequences $ \(num, (s, _)) -> describe (show num) $ do
-    seqData <- runIO $ getSequence' num
+    seqData <- runIO $ getSequence num
     it "should match the OEIS" $ do
       case seqData of
         Nothing -> pendingWith $ printf "Sequence A%d could not be fetched" num
